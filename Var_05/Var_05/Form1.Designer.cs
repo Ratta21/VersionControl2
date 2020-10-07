fb328a68673aca3ea9343b6a78b338fc8a6d9f20
Bányai Tamás <rattavok@gmail.com>
2020-10-07 17:52:22 +0200

    4

--- Var_05/Var_05/Form1.Designer.cs
+++ Var_05/Var_05/Form1.Designer.cs
@@ -29,12 +29,54 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.portfolioDataSet = new Var_05.PortfolioDataSet();
+            this.portfolioDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.portfolioDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(428, 425);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // portfolioDataSet
+            // 
+            this.portfolioDataSet.DataSetName = "PortfolioDataSet";
+            this.portfolioDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // portfolioDataSetBindingSource
+            // 
+            this.portfolioDataSetBindingSource.DataSource = this.portfolioDataSet;
+            this.portfolioDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource portfolioDataSetBindingSource;
+        private PortfolioDataSet portfolioDataSet;
     }
 }
 
