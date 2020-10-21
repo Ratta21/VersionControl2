9120e488910b7ef4859dcc3fcfd7be5e1be59da2
Bányai Tamás <rattavok@gmail.com>
2020-10-21 17:47:14 +0200

    3

---
+++ mikroszim/mikroszim/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszim.Entities
+{
+    class BirthProbability
+    {
+
+        public int age { get; set; }
+        public byte kidcount { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ mikroszim/mikroszim/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszim.Entities
+{
+    public class DeathProbability
+    {
+        public int kor { get; set; }
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+    }
+}
