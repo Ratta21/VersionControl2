9082b89bff1fab0881303fe9d2466c122a7c3f7b
Bányai Tamás <rattavok@gmail.com>
2020-10-21 19:08:16 +0200

    7

--- mikroszim/mikroszim/Form1.cs
+++ mikroszim/mikroszim/Form1.cs
@@ -82,6 +82,41 @@ namespace mikroszim
 
             return deathprobabilities;
         }
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.kor == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -92,6 +127,7 @@ namespace mikroszim
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
+
                 }
 
                 int nbrOfMales = (from x in Population
